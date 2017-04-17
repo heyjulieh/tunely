d4fc026e3496932972327f7eb396ca2c9c17d7c4
heyjulieh <heyjulieh@gmail.com>
2017-04-17 11:37:52 -0700

    on click edit works, not inputting proper text for genres

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -35,9 +35,71 @@ $(document).ready(function() {
 
   // delete album when its delete button is clicked
   $('#albums').on('click', '.delete-album', handleDeleteAlbumClick);
+  $('#albums').on('click', '.edit-album', handleAlbumEditClick);
+  $('#albums').on('click', '.save-album', handleSaveChangesClick);
+  });
 
-});
+  // when the edit button for an album is clicked
+  function handleAlbumEditClick(e) {
+    var $albumRow = $(this).closest('.album');
+    var albumId = $albumRow.data('album-id');
+    console.log('edit album', albumId);
 
+    // show the save changes button
+    $albumRow.find('.save-album').toggleClass('hidden');
+    // hide the edit button
+    $albumRow.find('.edit-album').toggleClass('hidden');
+
+
+    // get the album name and replace its field with an input element
+    var albumName = $albumRow.find('span.album-name').text();
+    $albumRow.find('span.album-name').html('<input class="edit-album-name" value="' + albumName + '"></input>');
+
+    // get the artist name and replace its field with an input element
+    var artistName = $albumRow.find('span.artist-name').text();
+    $albumRow.find('span.artist-name').html('<input class="edit-artist-name" value="' + artistName + '"></input>');
+
+    // get the releasedate and replace its field with an input element
+    var releaseDate = $albumRow.find('span.album-releaseDate').text();
+    $albumRow.find('span.album-releaseDate').html('<input class="edit-album-releaseDate" value="' + releaseDate + '"></input>');
+
+    var genres = $albumRow.find('span.album-genres').text();
+    $albumRow.find('span.album-genres').html('<input class="edit-album-genres" value="' + genres + '"></input>');
+  }
+
+  // after editing an album, when the save changes button is clicked
+  function handleSaveChangesClick(e) {
+    var albumId = $(this).parents('.album').data('album-id'); // $(this).closest would have worked fine too
+    var $albumRow = $('[data-album-id=' + albumId + ']');
+
+    var data = {
+      name: $albumRow.find('.edit-album-name').val(),
+      artistName: $albumRow.find('.edit-artist-name').val(),
+      releaseDate: $albumRow.find('.edit-album-releaseDate').val(),
+      genres: $albumRow.find('.edit-album-genres').val()
+    };
+    console.log('PUT/Editing data for album', albumId, 'with data', data);
+
+    $.ajax({
+      method: 'PUT',
+      url: '/api/albums/' + albumId,
+      data: data,
+      success: handleAlbumUpdatedResponse
+    });
+  }
+
+  function handleAlbumUpdatedResponse(data) {
+    console.log('response to update', data);
+
+    var albumId = data._id;
+    // scratch this album from the page
+    $('[data-album-id=' + albumId + ']').remove();
+    // and then re-draw it with the updates ;-)
+    renderAlbum(data);
+
+    // BONUS: scroll the change into view ;-)
+    $('[data-album-id=' + albumId + ']')[0].scrollIntoView();
+  }
 // when a delete button for an album is clicked
 function handleDeleteAlbumClick(e) {
   var albumId = $(this).parents('.album').data('album-id');
@@ -111,6 +173,7 @@ function renderAlbum(album) {
                 <button class='btn btn-primary add-song'>Add Song</button>
                 <button class='btn btn-danger delete-album'>Delete Album</button>
                 <button class='btn btn-info edit-album'>Edit Album</button>
+                <button class='btn btn-success save-album hidden'>Save Changes</button>
             </div>
           </div>
         </div>
